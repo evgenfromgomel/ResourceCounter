64880f6942daeb0fff71fc0306a51f1a3e200200
evgen <evgenfromgomel@gmail.com>
2014-01-14 18:39:09 +0300

    patch v 0.0.2  - add logic for count resources  - restructed system of items

--- ResourceCounter/script/lib/core.js
+++ ResourceCounter/script/lib/core.js
@@ -1,5 +1,53 @@
-function getResourceNumber() {
+function countResourceNumber(item) {
 	
+	var listOfRes = [];
+	
+	if(item.type == 'SrcIngr'){
+		listOfRes.push(item);
+	}
+	if(item.type == 'ItemIngr'){
+		processRecipe(listOfRes, item.recipe);
+	}
+	
+	return listOfRes;	
+};
+
+
+
+function processRecipe(recs, recipe){
+		var ingrs = recipe.ingridients;
+		for ( var i = 0; i < ingrs.length; i++) {
+			if(ingrs[i].type == 'SrcIngr'){
+				addToList(recs, ingrs[i]);
+			}
+			if(ingrs[i].type == 'ItemIngr'){
+				processRecipe(recs, ingrs[i].recipe);
+			}
+		}
+		
+}
+
+
+function addToList(arr, ingr) {
+	var isFound = false;
+	for ( var i = 0; i < arr.length; i++) {
+		if(arr[i].name == ingr.name){
+			arr[i].quantity += ingr.quantity;
+			isFound = true;
+			break;
+		}
+	}
+	if(isFound == false){
+		arr.push(ingr);
+	}
+}
+
+function printResourceSummary(item) {
+	var arr = countResourceNumber(item);
+	document.writeln("<br>Для производства : " + item.name + " потребуется");
+	for ( var i = 0; i < arr.length; i++) {
+		document.writeln("<br>" + arr[i].name + ": " + arr[i].quantity + "");
+	}
 }
 
 function printItem(item){
---
+++ ResourceCounter/script/lib/items.js
@@ -0,0 +1,56 @@
+var mc = (function() {
+	
+	//Recipes
+var rec = (function() {
+		return {
+			
+			plank: function(){
+				var recPlank = new Recipe("Доска");
+				recPlank.numOfOutput = 4;
+				recPlank.addIngridient(wood, 1);
+				return recPlank;
+			},
+		
+			stick: function(){
+				var recStick = new Recipe("Палка");
+				recStick.numOfOutput = 4;
+				recStick.addIngridient(plank, 2);
+				return recStick;
+			},
+		
+			pickaxe: function(ingr){
+				var recPickaxe = new Recipe("кирка");
+				recPickaxe.numOfOutput = 1;
+				recPickaxe.addIngridient(stick, 2);
+				if(ingr.name == 'Доска'){
+					recPickaxe.name = "Деревянная " + recPickaxe.name;
+					recPickaxe.addIngridient(wood, 3);
+				}
+				if(ingr.name == 'Булыжник'){
+					recPickaxe.name = "Каменная " + recPickaxe.name;
+					recPickaxe.addIngridient(cobblestone, 3);
+				}
+				return recPickaxe;
+			}
+		}
+		
+	})();	
+	
+var wood = new SrcIngr("Древесина");
+var cobblestone = new SrcIngr("Булыжник");
+var plank = new ItemIngr(rec.plank());
+var stick = new ItemIngr(rec.stick());
+var woodPickAxe = new ItemIngr(rec.pickaxe(plank));
+var stonePickAxe = new ItemIngr(rec.pickaxe(cobblestone));
+
+
+
+return {
+	wood : wood,
+	plank : plank,
+	stick : stick,
+	woodPickAxe : woodPickAxe,
+	stonePickAxe : stonePickAxe
+}
+
+})();
